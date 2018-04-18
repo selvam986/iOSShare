2ba79ecd62c7a87b0246dcf90bd593f0861883f5
Selvam Srinivasan <selvamsrinivasan.86@gmail.com>
2018-04-18 11:20:35 -0400

    Initial project commit

---
+++ iOSShare/iOSShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSShare
+//
+//  Created by MLS Discovery on 4/18/18.
+//  Copyright © 2018 GodShare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
